494327d6e6c66f6e020cc05caa94ba334f8c8157
StevenJV <steven@vorefamily.net>
2019-11-22 12:04:11 -0500

    first commit

---
+++ sjv.artoo/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Json;
+
+namespace sjv.artoo
+{
+
+    public class Program
+    {
+        public static IConfiguration Configuration => new ConfigurationBuilder()
+            .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+            .AddJsonFile("appsettings.json", false, true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .WriteTo.RollingFile(new JsonFormatter(),
+                    $"{Configuration["LogPath"]}{Configuration["ServiceName"]}.txt")
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ sjv.artoo/SjvArtoo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Serilog;
+using Topshelf;
+
+namespace sjv.artoo
+{
+    public class SjvArtoo : ServiceControl
+    {
+        private readonly IBusControl _bus;
+
+        public SjvArtoo(IBusControl bus)
+        {
+            _bus = bus;
+        }
+        public bool Start(HostControl hostControl)
+        {
+            _bus.Start();
+            Log.Logger.Information($"sjv.artoo started at: {DateTime.UtcNow}");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _bus?.Stop(TimeSpan.FromSeconds(30));
+            Log.Logger.Information($"sjv.artoo stopped at: {DateTime.UtcNow}");
+            return true;
+        }
+    }
+}
